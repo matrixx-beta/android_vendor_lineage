a6de1adb7c3548e44dca4f3570fbbe65cd94eee5
mukesh22584 <mks22584@gmail.com>
2024-06-06 12:27:05 +0530

    vendor: Enable quick tap for all
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- config/matrixx.mk
+++ config/matrixx.mk
@@ -57,7 +57,8 @@ endif
 
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml \
+    vendor/lineage/prebuilt/common/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
 
 # ColumbusService
 ifneq ($(TARGET_SUPPORTS_QUICK_TAP),false)
