ad0af113fca15075a7bbce0000d05d2f5f29579c
darkphnx <dark.phnx12@gmail.com>
2025-01-29 15:43:53 +0100

    vendor: Update Gms path
    Signed-off-by: darkphnx <dark.phnx12@gmail.com>

--- config/matrixx.mk
+++ config/matrixx.mk
@@ -39,11 +39,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # GAPPS
 ifeq ($(WITH_GMS),true)
-$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
+$(call inherit-product, vendor/gms/products/gms.mk)
 $(call inherit-product, vendor/google/overlays/ThemeIcons/config.mk)
-PRODUCT_PACKAGES += OtaGapps
-else
-PRODUCT_PACKAGES += OtaVanila
 endif
 
 # Cloned app exemption
