7335b38808266f458e3ae26d61421b2f3bd048f7
mukesh22584 <mks22584@gmail.com>
2025-04-29 19:34:58 +0530

    Revert "vendor: Enable quick tap for all"
    This reverts commit a7e91f450961383305f4789ccc1cf2e874ae0a4e.

--- config/matrixx.mk
+++ config/matrixx.mk
@@ -62,8 +62,7 @@ endif
 
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml \
-    vendor/lineage/prebuilt/common/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml
 
 # ColumbusService
 ifneq ($(TARGET_SUPPORTS_QUICK_TAP),false)
